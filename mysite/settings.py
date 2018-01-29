6f87c73df43c33bfa5f831e812529adc6c27ebd7
ShoutingCat <wvimivw@gmail.com>
2018-01-30 01:12:25 +0900

    블로그 기능 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'polls',
     'books',
     'introduce',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -110,9 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+#TIME_ZONE = 'UTC'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ko-kr'
+TIME_ZONE = 'Asia/Seoul'
+
 
 USE_I18N = True
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^polls/', include('polls.urls')),
 	url(r'^books/', include('books.urls')),
-	url(r'^introduce/', include('introduce.urls')),
+	# url(r'^introduce/', include('introduce.urls')),
 	url(r'^', include('introduce.urls')),
+	url(r'^blog/', include('blog.urls')),
 ]
